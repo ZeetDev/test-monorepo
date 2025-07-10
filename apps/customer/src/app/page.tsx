973331cabc3ff1bb76e6f2f2747e037b238c5a03
ZeetDev <zeetpatel2712@gmail.com>
2025-07-10 21:48:41 +0530

    remove old styles

--- apps/customer/src/app/page.tsx
+++ apps/customer/src/app/page.tsx
@@ -1,7 +1,3 @@
-export default function Index() {
-  return (
-    <div className="bg-green-500">
-      <h1 className="text-3xl font-bold underline p-5">Hello Customer!</h1>
-    </div>
-  );
+export default function Customer() {
+  return <h1>Customer</h1>;
 }
